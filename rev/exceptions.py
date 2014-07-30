6f96d8d16117aca42aac8023895695a7ff3e84fd
Sebastien Dupont <seb@koemei.com>
2014-07-30 12:24:21 +0200

    add notification callback url to order requests + some refactoring

--- rev/exceptions.py
+++ rev/exceptions.py
@@ -1,6 +1,20 @@
 
 class SettingsFileNotFoundError(Exception):
+    """
+    Settings file was not found
+    """
+
     def __init__(self, message):
 
+        # Call the base class constructor with the parameters it needs
+        Exception.__init__(self, message)
+
+
+class OrderNotFoundError(Exception):
+    """
+    Order with a given external ref was not found on rev api
+    """
+
+    def __init__(self, message):
         # Call the base class constructor with the parameters it needs
         Exception.__init__(self, message)
